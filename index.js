9105e3aca8a922286034051a05d75573e71d38a8
Kumaravel Palanisamy <kumar.okm1995@gmail.com>
2020-05-31 18:29:42 +0530

    converted class into function

--- index.js
+++ index.js
@@ -2,52 +2,10 @@ const parsePhoneNumber = require('libphonenumber-js');
 const moment = require('moment-timezone');
 const dial = require("./data/dial.json");
 
-class PhoneTime {
+module.exports = function phoneNumberNow(phoneNumber) {
+    var tzInCapital;
 
-    getData(phoneNumber) {
-        var tzInCapital;
-
-        const countryCode = this._parsePhoneNumber(phoneNumber);
-        const data = dial[countryCode.toString()] || [];
-        if (data.length > 0) {
-            for (var i in data) {
-                var dat = data[i];
-                if(dat.updated) {
-                    console.log("skipping")
-                    continue;
-                }
-                dat = {
-                    ...dat,
-                    "code": 200,
-                    "msg": "OK"
-                };
-                var tzInCapital = dat.tzInCapital || "";
-                if (tzInCapital) {
-                    dat.moment = moment().tz(tzInCapital);
-                } else {
-                    dat = {
-                        ...dat,
-                        "code": 404,
-                        "msg": "TimeZone not found for given country code"
-                    };  
-                }
-                if (dat.moment) {
-                    dat.timeDiff = this._getLocalTimeZone(tzInCapital);
-                } else {
-                    dat = {
-                        ...dat,
-                        "code": 404,
-                        "msg": "TimeZone is invalid"
-                    };
-                }
-                dat.updated = true;
-                data[i]= dat;
-            }
-        }
-        return data;
-    }
-
-    _parsePhoneNumber(phoneNumber) {
+    this._parsePhoneNumber = function(phoneNumber) {
         var countryCode = "";
         try {
             const pNumber = parsePhoneNumber.parsePhoneNumberFromString(phoneNumber);
@@ -57,15 +15,50 @@ class PhoneTime {
         }
         return countryCode;
     }
-
-    _getLocalTimeZone(tzInCapital) {
+    
+    this._getLocalTimeZone = function(tzInCapital) {
         var now = moment();
         var localOffset = now.utcOffset();
         now.tz(tzInCapital);
         return localOffset - now.utcOffset();
     }
-}
 
-module.exports = {
-    PhoneTime
-};
+    const countryCode = this._parsePhoneNumber(phoneNumber);
+    const data = dial[countryCode.toString()] || [];
+    if (data.length > 0) {
+        for (var i in data) {
+            var dat = data[i];
+            if (dat.updated) {
+                continue;
+            }
+            dat = {
+                ...dat,
+                "code": 200,
+                "msg": "OK"
+            };
+            var tzInCapital = dat.tzInCapital || "";
+            if (tzInCapital) {
+                dat.moment = moment().tz(tzInCapital);
+            } else {
+                dat = {
+                    ...dat,
+                    "code": 404,
+                    "msg": "TimeZone not found for given country code"
+                };
+            }
+            if (dat.moment) {
+                dat.timeDiff = this._getLocalTimeZone(tzInCapital);
+            } else {
+                dat = {
+                    ...dat,
+                    "code": 404,
+                    "msg": "TimeZone is invalid"
+                };
+            }
+            dat.updated = true;
+            data[i] = dat;
+        }
+    }
+    return data;
+    
+}
